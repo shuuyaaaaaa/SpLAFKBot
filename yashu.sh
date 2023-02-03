befd3e7854e825aa0dcd5ce628f20706578b5270
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 11:58:13 +0530

    Update yashu.sh

--- yashu.sh
+++ yashu.sh
@@ -1,7 +1,6 @@
-"U2FsdGVkX18LDA16WR+WhtzUuQxtS+H8hZfRDvKQdhHWm7lep8sy3dk+cxR+7Q9C
-mkFlSYxfwuJqw83h4oTJwA=="
+R = "U2FsdGVkX18LDA16WR+WhtzUuQxtS+H8hZfRDvKQdhHWm7lep8sy3dk+cxR+7Q9CmkFlSYxfwuJqw83h4oTJwA==" | openssl aes-256-cbc -d -a -pass pass:somepassword
 python3 -m pip install -r requirements.txt
 python3 env_vars.py
-git clone $REPO AFK
+git clone $R AFK
 cd AFK
 python3 yashu.py
