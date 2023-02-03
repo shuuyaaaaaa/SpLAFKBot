f5506a66c82596477265e81c6c7dd1662dff6e26
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 12:09:40 +0530

    Update yashu.sh

--- yashu.sh
+++ yashu.sh
@@ -1,4 +1,4 @@
-R = "U2FsdGVkX18LDA16WR+WhtzUuQxtS+H8hZfRDvKQdhHWm7lep8sy3dk+cxR+7Q9CmkFlSYxfwuJqw83h4oTJwA==" | openssl aes-256-cbc -d -a -pass pass:somepassword
+export R="U2FsdGVkX18LDA16WR+WhtzUuQxtS+H8hZfRDvKQdhHWm7lep8sy3dk+cxR+7Q9CmkFlSYxfwuJqw83h4oTJwA==" | openssl aes-256-cbc -d -a -pass pass:somepassword
 python3 -m pip install -r requirements.txt
 python3 env_vars.py
 git clone $R AFK
