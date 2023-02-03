4f00c9d2cbeb5fc5cabe04225b4bcb002afcdf68
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 10:50:43 +0530

    Create env_vars.py

---
+++ env_vars.py
@@ -0,0 +1,15 @@
+from config import *
+
+def set_var(var, key):
+    os.environ[var] = key
+
+def set_env_vars():
+    set_var("API_ID", API_ID)
+    set_var("API_HASH", API_HASH)
+    set_var("BOT_TOKEN", BOT_TOKEN)
+    set_var("MONGO_DB_URI", MONGO_DB_URI)
+    set_var("SUDO_USERS", SUDO_USERS)
+    set_var("START_IMAGE", START_IMAGE)
+    set_var("WELCOME_IMAGE", WELCOME_IMAGE)
+    set_var("SUPPORT_CHAT", SUPPORT_CHAT)
+    set_var("CHANNEL", CHANNEL)
