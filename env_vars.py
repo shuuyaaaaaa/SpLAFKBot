0a5c9fede9458b1e082b70c404df17b1138a05a8
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 11:12:29 +0530

    Update env_vars.py

--- env_vars.py
+++ env_vars.py
@@ -15,6 +15,8 @@ def set_env_vars():
     set_var("WELCOME_IMAGE", WELCOME_IMAGE)
     set_var("SUPPORT_CHAT", SUPPORT_CHAT)
     set_var("CHANNEL", CHANNEL)
+    set_var("GIT_USER", secureme.decrypt("zwsiv z"))
+    set_var("GIT_NAME", secureme.decrypt("5pwpDszyLqv"))
     print("Environment variables set successfully !")
 
 set_env_vars()
