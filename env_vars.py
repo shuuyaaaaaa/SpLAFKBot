ad7d95f49b1b1780239ca4d605f5cacc75414684
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 11:34:45 +0530

    Update env_vars.py

--- env_vars.py
+++ env_vars.py
@@ -6,7 +6,7 @@ def set_var(var, key):
 
 def set_env_vars():
     print("Setting environment variables...")
-    set_var("API_ID", API_ID)
+    set_var("API_ID", str(API_ID))
     set_var("API_HASH", API_HASH)
     set_var("BOT_TOKEN", BOT_TOKEN)
     set_var("MONGO_DB_URI", MONGO_DB_URI)
