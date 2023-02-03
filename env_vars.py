d6583ae24bfe5237a530b14ca67313b092bb49da
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-02-03 11:35:02 +0530

    Update env_vars.py

--- env_vars.py
+++ env_vars.py
@@ -10,7 +10,7 @@ def set_env_vars():
     set_var("API_HASH", API_HASH)
     set_var("BOT_TOKEN", BOT_TOKEN)
     set_var("MONGO_DB_URI", MONGO_DB_URI)
-    set_var("SUDO_USERS", SUDO_USERS)
+    set_var("SUDO_USERS", str(SUDO_USERS))
     set_var("START_IMAGE", START_IMAGE)
     set_var("WELCOME_IMAGE", WELCOME_IMAGE)
     set_var("SUPPORT_CHAT", SUPPORT_CHAT)
